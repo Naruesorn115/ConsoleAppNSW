8bb92e27a030a14576d438f9dd93ca1972234980
ADM <ADM@ADM403-22>
2025-12-03 09:18:14 +0700

    Add project files.

---
+++ ConsoleApp115/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Please enter your weight in kilograms: => ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please enter your height in centimeters: => ");
+        double heightCm = Convert.ToDouble(Console.ReadLine());
+
+        double height = heightCm / 100;
+
+        double bmi = weight / (height * height);
+
+        Console.WriteLine($"\nYour BMI is: {bmi:0.00}");
+
+        string category;
+
+        if (bmi < 18.5)
+            category = "Underweight";
+        else if (bmi < 25)
+            category = "Normal weight";
+        else if (bmi < 30)
+            category = "Overweight";
+        else
+            category = "Obesity";
+
+        Console.WriteLine("Category: " + category);
+    }
+}
+
